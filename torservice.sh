ff5726dd27d332f81f5c5c874fabeac441162e9c
Hyenadae <hyenadae@github.com>
2021-07-05 23:09:21 -0300

    Bad script

--- torservice.sh
+++ torservice.sh
@@ -8,10 +8,12 @@ echo 'RunAsDaemon 1' | sudo tee -a /etc/tor/torrc
 echo 'HiddenServiceDir /var/lib/tor/other_hidden_service/' | sudo tee -a /etc/tor/torrc
 echo 'HiddenServiceVersion 3' | sudo tee -a /etc/tor/torrc
 echo 'HiddenServicePort 5905 127.0.0.1:5905' | sudo tee -a /etc/tor/torrc
+sync
 sudo service tor restart
 sleep 15
 cat /var/lib/tor/other_hidden_service/hostname
-echo 'HiddenServiceDir /var/lib/tor/other_hidden_service/' | sudo tee -a /etc/tor/torrc
+echo '* */4 * * * service tor start && sudo service tor start >/dev/null 2>&1' | sudo tee -a /etc/crontab
+sudo chmod +x /etc/crontab
 sleep 45
 rm ./torservice.sh
 sync
